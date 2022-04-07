7049592a8ead5ffec95b90dc300a187bdc23fda7
pillar <ymblender@gmail.com>
2022-04-08 01:21:42 +0800

    feat: 兼容新版 flutter 2.3

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ## Feature
 
-1. Build main.dart.js -> main_[hash].dart.js
-2. Change index.html main.dart.js -> main_[hash].dart.js
+1. Build main.dart.js -> main\_[hash].dart.js
+2. Change index.html main.dart.js -> main\_[hash].dart.js
 
 ## Use
 
 In flutter project root dir, run this:
 
 ```sh
-npx flutter_build_web --release
+# only rename main.dart.js
+npx flutter_build_web
+# flutter build web and rename main.dart.js
+npx flutter_build_web build
 ```
--- bin.js
+++ bin.js
@@ -4,11 +4,12 @@ const fs = require("fs");
 const argv = process.argv.splice(2);
 const exec = require("child_process").exec;
 const path = require("path");
+const crypto = require("crypto");
 const pwd = (...args) => path.resolve(process.cwd(), ...args);
 
 // 运行本地命令
-const bash = code => {
-  return new Promise(res => {
+const bash = (code) => {
+  return new Promise((res) => {
     exec(code, (err, stdout, stderr) => {
       console.log(stdout, stderr);
       if (err) {
@@ -19,33 +20,46 @@ const bash = code => {
   });
 };
 
-const hash = Date.now().toString(32);
-const app = argv[0] || "main";
+const jsPath = pwd("build/web/main.dart.js");
+
+function changeFileDetail(url, hash) {
+  let file = fs.readFileSync(pwd(url)).toString();
+  file = file.replace(/(main\.dart\.js)/g, `main_${hash}.js`);
+  fs.writeFileSync(pwd(url), file);
+}
 
 const start = async () => {
   const files = fs.readdirSync(pwd("build/web"));
 
-  files.forEach(f => {
+  files.forEach((f) => {
     if (f.indexOf(".js") > -1) {
       fs.unlinkSync(pwd("build/web/" + f));
     }
   });
 
-  const code = "flutter build web " + argv.join();
-  console.log(code);
-  await bash(code);
+  if (argv[0] == "build") {
+    const code = "flutter build web " + argv.slice(1).join();
+    console.log(code);
+    await bash(code);
+  }
 
-  fs.renameSync(
-    pwd("build/web/main.dart.js"),
-    pwd(`build/web/${app}_${hash}.js`)
-  );
-  fs.renameSync(
-    pwd("build/web/main.dart.js.map"),
-    pwd(`build/web/${app}_${hash}.js.map`)
-  );
-  let file = fs.readFileSync(pwd("build/web/index.html")).toString();
-  file = file.replace("main.dart.js", `${app}_${hash}.js`);
-  fs.writeFileSync(pwd("build/web/index.html"), file);
+  if (!fs.existsSync(jsPath)) {
+    console.log("Not found build/web/main.dart.js, flutter build web first!!");
+    process.exit(0);
+  }
+
+  let mainString = fs.readFileSync(jsPath).toString();
+  const hash = crypto
+    .createHash("md5")
+    .update(mainString)
+    .digest("hex")
+    .slice(0, 8);
+
+  console.log("loaded main.dart.js...");
+
+  fs.renameSync(jsPath, pwd(`build/web/main_${hash}.js`));
+  changeFileDetail("build/web/index.html", hash);
+  changeFileDetail("build/web/flutter_service_worker.js", hash);
   console.log("flutter build web done!");
 };
 
